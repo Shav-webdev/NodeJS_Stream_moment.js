810dcea08a1aa3b5da471f767309a31b1c1394a1
Shavarsh <shavarsh.abrahamyan1993@gmail.com>
2020-01-30 03:51:35 +0400

    1.created node_stream project, 2.installed moment.js with npm, 3.created class MyWritableStrem and implemented it's write method, which writes to a stream every second the current time

---
+++ index.js
@@ -0,0 +1,42 @@
+const {Writable} = require("stream");
+const fs = require("fs");
+const moment = require("moment");
+
+/**
+ * @method _write of @class MyWritableStream write to a stream every second
+ * the current time formatted using moment.js and write that stream to the file*/
+
+class MyWritableStream extends Writable {
+    constructor(sPath) {
+        super();
+        this.wStream = fs.createWriteStream(`${sPath}`)
+        this.counter = 0;
+    }
+    _write(chunk, enc, next) {
+        if (chunk.length === 0) {
+            next(new Error('empty chunk detected'));
+        } else {
+            setInterval(() => {
+                ++this.counter;
+                console.log(this.counter)
+                this.wStream.write(`${this.counter}: ${chunk} ${moment().format('MMMM Do YYYY, h:mm:ss a')} \n`);
+            }, 1000);
+        }
+        next();
+    }
+}
+
+const ws = new MyWritableStream("./text.txt");
+
+
+ws._write("now", "utf8", function (err) {
+    if (err){
+        console.log("err", err.message)
+    }
+});
+
+fs.createReadStream("./text.txt").pipe(ws);
+
+
+
+
